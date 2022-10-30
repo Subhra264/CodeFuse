194436058a1691c75e1e5fc5f675e2ce06cc5aa1
Abhra303 <chakrabortyabhradeep79@gmail.com>
2022-10-30 14:47:34 +0530

    teach server to response on dynamic code input

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,18 @@ const openai = new OpenAIApi(configuration);
 
 app.use(Express.json());
 
-app.get('/', async (req: Request, res: Response) => {
+app.post('/', async (req: Request, res: Response) => {
+  const { code } = req.body;
+
+  if (!code) {
+    return res.json({
+      message: 'Code required'
+    });
+  }
+
   const response = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: "class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.",
+    prompt: code + "\"\"\"\nHere's what the above code is doing:\n1.",
     temperature: 0,
     max_tokens: 64,
     top_p: 1.0,
